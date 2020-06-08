6cae74bcf8015b77e61aafd56e31eea1e3726c5f
DemoCode6 <13552553827@163.com>
2020-06-08 17:39:15 +0800

    小程序的下拉列表组件

---
+++ pages/selece_test/selece_test.js
@@ -0,0 +1,40 @@
+// pages/district/district.js
+Page({
+  data: {
+
+    selectArray: [
+      {
+        "id": "10",
+        "value": "爆款商品"
+      },
+      {
+        "id": "10",
+        "value": "会计类"
+      },
+      {
+        "id": "21",
+        "value": "工程类"
+      }, 
+      ],
+    selectArray1: [
+      {
+        "id": "10",
+        "value": "超值服务"
+      },
+      {
+        "id": "10",
+        "value": "会计类1"
+      },
+      {
+        "id": "21",
+        "value": "工程类1"
+      }, 
+      ],
+  },
+
+  select: function(e) {
+      //  console.log(e.detail)
+    },
+
+
+})
